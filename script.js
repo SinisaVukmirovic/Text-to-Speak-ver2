f6a9d76d9d92c5a1521ae932df56a8da5972b777
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-05-06 18:14:08 +0200

    Finished functionality

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const stopBtn = document.getElementById('stopBtn');
 const textInput = document.getElementById('textInput');
 const speedInput = document.getElementById('speedInput');
 
+// keeping track of current character so that we can know where the speech is at,
+// if we change the speed rate while speaking
+let currentChar;
+
 playBtn.addEventListener('click', () => {
     speakText(textInput.value);
 });
@@ -13,6 +17,26 @@ pauseBtn.addEventListener('click', pauseSpeech);
 
 stopBtn.addEventListener('click', stopSpeech);
 
+speedInput.addEventListener('input', () => {
+    stopSpeech();
+
+    if (speechSynthesis.speaking) return;
+    // continuing speech from the character on which the speed rate was changed
+    speakText(utterance.text.substring(currentChar));
+});
+
+// for speaking we will use a speach API
+const utterance = new SpeechSynthesisUtterance();
+
+// anabling text input when speach is done
+utterance.addEventListener('end', () => {
+    textInput.disabled = false;
+});
+
+utterance.addEventListener('boundary', e => {
+    currentChar = e.charIndex;
+});
+
 function speakText(text) {
     // checking if speach is in progress and handling resule
     if (speechSynthesis.paused && speechSynthesis.speaking) {
@@ -20,15 +44,12 @@ function speakText(text) {
         return speechSynthesis.resume();
     }
 
-    // for speaking we will use a speach API
-    const utterance = new SpeechSynthesisUtterance(text);
+    // checking if speech is in progress to prevent duble playing is play btn is clicked again
+    if (speechSynthesis.speaking) return;
+
+    utterance.text = text;
 
     utterance.rate = speedInput.value || 1;
-
-    // anabling text input when speach is done
-    utterance.addEventListener('end', () => {
-        textInput.disabled = false;
-    });
 
     // disabling text input while speaking
     textInput.disabled = true;
